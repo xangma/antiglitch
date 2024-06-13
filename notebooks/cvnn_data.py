41e5188fa450bffb321f69bc22c237548224a3c2
xangma <xangma@users.noreply.github.com>
2024-06-13 16:07:33 +0100

    lots of fixes

--- notebooks/cvnn_data.py
+++ notebooks/cvnn_data.py
@@ -14,6 +14,8 @@ import torch
 import antiglitch
 from antiglitch.utils import to_fd
 
+rng = np.random.default_rng(seed=0)
+
 def get_data(datadir):
 	# Load data
 	# files are named as follows: ifo-key-num.npz,
@@ -99,17 +101,77 @@ def get_data(datadir):
 					distributions[ifo][ml_model][key].append(glitches[ifo][ml_model][glitch_num][key])
 	return distributions, glitches, ifos, ml_models
 
+def noise_function(x):
+	noise_generator_function = lambda x: np.random.normal(0, 1, len(x)) + 1j * np.random.normal(0, 1, len(x))
+	noise = np.array([noise_generator_function(x) for x in x])
+	x += noise
+	return x
+
+def phase_shift(x, shift=np.pi):
+	"""Multiply by e^i*shift"""
+	phase_shifts = []
+	for i in range(len(x)):
+		phase_shift = rng.choice([-np.pi, 0, np.pi],)
+		if phase_shift != 0:
+			x[i] = x[i] * np.exp(1j * phase_shift)
+		phase_shifts.append(phase_shift)
+	return x, phase_shifts
+
+def time_shift(x, shift=0):
+	"""Multiply by e^-j*2*np.pi*f*t"""
+	time_shifts = []
+	farray = np.linspace(0, 4096, 513) * 4096
+	for i in range(len(x)):
+		time_shift = rng.uniform(-0.1, 0.1)
+		x[i] = x[i] * np.exp(-1j * farray * time_shift)
+		time_shifts.append(time_shift)
+	return x, time_shifts
+
+def normalize(x, dryrun=False):
+	# check if complex or real
+	if np.iscomplexobj(x):
+		real_parts = np.real(x)
+		imag_parts = np.imag(x)
+		x_mean_real = np.mean(real_parts)
+		x_std_real = np.std(real_parts)
+		x_mean_imag = np.mean(imag_parts)
+		x_std_imag = np.std(imag_parts)
+		normalized_real = (real_parts - x_mean_real) / x_std_real
+		normalized_imag = (imag_parts - x_mean_imag) / x_std_imag
+		print(f"x_mean_real: {x_mean_real}, x_std_real: {x_std_real}, x_mean_imag: {x_mean_imag}, x_std_imag: {x_std_imag}")
+		if not dryrun:
+			x = normalized_real + 1j * normalized_imag
+		return x, x_mean_real, x_std_real, x_mean_imag, x_std_imag
+	else:
+		x_mean = x.mean()
+		x_std = x.std()
+		if not dryrun:
+			x = (x - x_mean) / x_std
+		print(f"x_mean: {x_mean}, x_std: {x_std}")
+		return x, x_mean, x_std
+
 class GlitchDataset(torch.utils.data.TensorDataset):
-	def __init__(self, datadir, ifos, ml_models, glitches, distributions, tr_size, te_size, device, noise=True, aug=True, split='train', outtype='complex', cachedataset_prefix="antiglitch_cvnn_dataset_clean"):
+	def __init__(self, datadir, ifos, ml_models, glitches, distributions, tr_size, te_size, device, noise=True, aug_phase=True, aug_time=True, split='train', outtype='complex', train_data=None, cachedataset_prefix="antiglitch_cvnn_dataset_clean"):
+		self.datadir = datadir
 		self.ifos = ifos
 		self.ml_models = ml_models
 		self.glitches = glitches
 		self.distributions = distributions
 		self.tr_size = tr_size
 		self.te_size = te_size
+		self.noise = noise
+		self.aug_phase = aug_phase
+		self.aug_time = aug_time
+		self.device = device
+		self.cachedataset_prefix = cachedataset_prefix
 		self.size = tr_size if split == 'train' else te_size
-		self.x_arr = []
-		self.y_arr = []
+		self.x_arr, self.y_arr = None, None
+		self.tr_x_mean_real, self.tr_x_std_real, self.tr_x_mean_imag, self.tr_x_std_imag = None, None, None, None
+		self.tr_x_mean, self.tr_x_std = None, None
+		self.tr_y1_mean, self.tr_y1_std = None, None
+		self.tr_y2_mean, self.tr_y2_std = None, None
+		self.tr_phase_shifts = None
+		self.te_phase_shifts = None
 		tr_x_arr_irfft = None
 		# create datasets if they don't exist
 		for size in [self.tr_size, self.te_size]:
@@ -123,128 +185,91 @@ class GlitchDataset(torch.utils.data.TensorDataset):
 				y_arr = np.array(y_arr)
 				# normalize x_arr
 				np.savez(datadir + cachedataset_prefix + str(size) + '.npz', x_arr=x_arr, y_arr=y_arr)
-		tr_data = np.load(datadir + cachedataset_prefix + str(self.tr_size) + '.npz')
-		if split == 'test':
-			te_data = np.load(datadir + cachedataset_prefix + str(self.te_size) + '.npz')
-   
-   
-		tr_x_arr = tr_data['x_arr']
-		if split == 'test':
-			te_x_arr = te_data['x_arr']
-  
-		# add noise to x_arr
-		if noise:
-			# create white noise in the frequency domain
-			noise_generator_function = lambda x: np.random.normal(0, 1, len(x)) + 1j * np.random.normal(0, 1, len(x))
-			noise = np.array([noise_generator_function(x) for x in tr_x_arr])
-			tr_x_arr += noise
-			del noise
-			if split == 'test':
-				noise = np.array([noise_generator_function(x) for x in te_x_arr])
-				te_x_arr += noise
-				del noise
-		
-		if aug:
-			# apply random phase shifts to x_arr
-			self.tr_phase_shifts = np.random.uniform(-np.pi/4, np.pi/4, len(tr_x_arr))
 
-			# Apply the phase shifts to the frequency domain signals
-			for i in range(len(tr_x_arr)):
-				tr_x_arr[i] = tr_x_arr[i] * np.exp(1j * self.tr_phase_shifts[i])
-			if split == 'test':
-				self.te_phase_shifts = np.random.uniform(-np.pi/4, np.pi/4, len(te_x_arr))
-				for i in range(len(te_x_arr)):
-					te_x_arr[i] = te_x_arr[i] * np.exp(1j * self.te_phase_shifts[i])
-
-		# normalize training x_arr
-  
-		if outtype == 'complex':
-			# normalize training x_arr
-			tr_real_parts = np.real(tr_x_arr)
-			tr_imag_parts = np.imag(tr_x_arr)
-			
-			tr_x_mean_real = np.mean(tr_x_arr.real)
-			tr_x_std_real = np.std(tr_x_arr.real)
-			tr_x_mean_imag = np.mean(tr_x_arr.imag)
-			tr_x_std_imag = np.std(tr_x_arr.imag)
-			self.x_arr_mean_real = tr_x_mean_real
-			self.x_arr_std_real = tr_x_std_real
-			self.x_arr_mean_imag = tr_x_mean_imag
-			self.x_arr_std_imag = tr_x_std_imag
-			tr_normalized_real = (tr_real_parts - tr_x_mean_real) / tr_x_std_real
-			tr_normalized_imag = (tr_imag_parts - tr_x_mean_imag) / tr_x_std_imag
-			
-			self.x_arr = tr_normalized_real + 1j * tr_normalized_imag
-
-			print(f"tr_x_mean_real: {tr_x_mean_real}, tr_x_std_real: {tr_x_std_real}, tr_x_mean_imag: {tr_x_mean_imag}, tr_x_std_imag: {tr_x_std_imag}")
-			if split == 'test':
-				# normalize test x_arr with training scalings
-				te_real_parts = np.real(te_x_arr)
-				te_imag_parts = np.imag(te_x_arr)
-				te_x_mean_real = np.mean(te_x_arr.real)
-				te_x_std_real = np.std(te_x_arr.real)	
-				te_x_mean_imag = np.mean(te_x_arr.imag)
-				te_x_std_imag = np.std(te_x_arr.imag)
-				print(f"te_x_mean_real: {te_x_mean_real}, te_x_std_real: {te_x_std_real}, te_x_mean_imag: {te_x_mean_imag}, te_x_std_imag: {te_x_std_imag}")
-				te_normalized_real = (te_real_parts - tr_x_mean_real) / tr_x_std_real
-				te_normalized_imag = (te_imag_parts - tr_x_mean_imag) / tr_x_std_imag
-				self.x_arr = te_normalized_real + 1j * te_normalized_imag
-				del te_real_parts, te_imag_parts, te_normalized_real, te_normalized_imag
-			del tr_real_parts, tr_imag_parts, tr_normalized_real, tr_normalized_imag, tr_x_mean_real, tr_x_std_real, tr_x_mean_imag, tr_x_std_imag
-			self.x_arr = torch.tensor(self.x_arr, device=device, dtype=torch.complex64)
-
-		elif outtype == 'real':
-			# irfft to get to time domain
-			len_tr_x_arr = len(tr_x_arr)
-			len_tr_x_samp = len(tr_x_arr[0])
-			if 'tr_x_arr_irfft' not in locals():
+		if split == 'train':
+			tr_data = np.load(datadir + cachedataset_prefix + str(self.tr_size) + '.npz')
+			tr_x_arr = tr_data['x_arr']
+			if noise:
+				# create white noise in the frequency domain
+				tr_x_arr = noise_function(tr_x_arr)
+			if aug_phase:
+				# Apply the phase shifts to the frequency domain signals
+				tr_x_arr, self.tr_phase_shifts = phase_shift(tr_x_arr)
+			if aug_time:
+				# Apply the time shifts to the frequency domain signals
+				tr_x_arr, self.tr_time_shifts = time_shift(tr_x_arr)
+			if outtype == 'complex':
+				print("Normalizing x train data (complex)")
+				self.x_arr, self.tr_x_mean_real, self.tr_x_std_real, self.tr_x_mean_imag, self.tr_x_std_imag = normalize(tr_x_arr)
+				self.x_arr = torch.tensor(self.x_arr, device=device, dtype=torch.complex64)
+			elif outtype == 'real':
+				# irfft to get to time domain
+				len_tr_x_arr = len(tr_x_arr)
+				len_tr_x_samp = len(tr_x_arr[0])
 				tr_x_arr_irfft = np.array([np.roll(irfft(x), len_tr_x_samp//2) for x in tr_x_arr])
-			# normalize training x_arr
-			tr_x_mean_irfft = tr_x_arr_irfft.mean()
-			self.x_arr_mean = tr_x_mean_irfft
-			tr_x_std_irfft = tr_x_arr_irfft.std()
-			self.x_arr_std = tr_x_std_irfft
-			self.x_arr = (tr_x_arr_irfft - tr_x_mean_irfft) / tr_x_std_irfft
-			self.x_arr = torch.tensor(self.x_arr, device=device, dtype=torch.float32)
-
-			if split == 'test':
+				print("Normalizing x train data (real)")
+				self.x_arr, self.tr_x_mean, self.tr_x_std = normalize(tr_x_arr_irfft)
+				self.x_arr = torch.tensor(self.x_arr, device=device, dtype=torch.float32)
+    
+			# normalize training y_arr
+			tr_y_arr = tr_data['y_arr']
+			print("Normalizing y train data (y1)")
+			tr_y_arr[:,0], self.tr_y1_mean, self.tr_y1_std = normalize(tr_y_arr[:,0])
+			print("Normalizing y train data (y2)")
+			tr_y_arr[:,1], self.tr_y2_mean, self.tr_y2_std = normalize(tr_y_arr[:,1])
+			self.y_arr = tr_y_arr
+			self.y_arr = torch.tensor(self.y_arr, device=device, dtype=torch.float32)
+   
+		if split == 'test':
+			# check train_data has been passed
+			if train_data is None:
+				raise ValueError("train_data must be passed if split is 'test'")
+			te_data = np.load(datadir + cachedataset_prefix + str(self.te_size) + '.npz')
+			te_x_arr = te_data['x_arr']
+			if noise:
+				# create white noise in the frequency domain
+				te_x_arr = noise_function(te_x_arr)
+			if aug_phase:
+				# Apply the phase shifts to the frequency domain signals
+				te_x_arr, self.te_phase_shifts = phase_shift(te_x_arr)
+			if aug_time:
+				# Apply the time shifts to the frequency domain signals
+				te_x_arr, self.te_time_shifts = time_shift(te_x_arr)
+			if outtype == 'complex':
 				# normalize test x_arr with training scalings
+				print("Normalizing x test data (complex)")
+				normalize(te_x_arr, dryrun=True)
+				norm_real = (te_x_arr.real - train_data.tr_x_mean_real) / train_data.tr_x_std_real
+				norm_imag = (te_x_arr.imag - train_data.tr_x_mean_imag) / train_data.tr_x_std_imag
+				self.x_arr = norm_real + 1j * norm_imag
+				self.x_arr = torch.tensor(self.x_arr, device=device, dtype=torch.complex64)
+			elif outtype == 'real':
+				# irfft to get to time domain
 				len_te_x_arr = len(te_x_arr)
 				len_te_x_samp = len(te_x_arr[0])
-				if 'te_x_arr_irfft' not in locals():
-					te_x_arr_irfft = np.array([np.roll(irfft(x), len_te_x_samp//2) for x in te_x_arr])
-				te_x_arr = (te_x_arr_irfft - tr_x_mean_irfft) / tr_x_std_irfft
-				self.x_arr = torch.tensor(te_x_arr, device=device, dtype=torch.float32)
-				del te_x_arr
-
-		# normalize training y_arr
-		tr_y_arr = tr_data['y_arr']
-		tr_y1_mean = tr_y_arr[:,0].mean()
-		tr_y1_std = tr_y_arr[:,0].std()
-		tr_y2_mean = tr_y_arr[:,1].mean()
-		tr_y2_std = tr_y_arr[:,1].std()
-
-		tr_y_arr[:,0] = (tr_y_arr[:,0] - tr_y1_mean)/tr_y1_std
-		tr_y_arr[:,1] = (tr_y_arr[:,1] - tr_y2_mean)/tr_y2_std
-		self.y_arr = tr_y_arr
-		if split == 'test':
+				te_x_arr_irfft = np.array([np.roll(irfft(x), len_te_x_samp//2) for x in te_x_arr])
+				# normalize test x_arr with training scalings
+				print("Normalizing x test data (real)")
+				normalize(te_x_arr_irfft, dryrun=True)
+				self.x_arr = (te_x_arr_irfft - train_data.tr_x_mean) / train_data.tr_x_std
+				self.x_arr = torch.tensor(self.x_arr, device=device, dtype=torch.float32)
+    
 			# normalize test y_arr with training scalings
 			te_y_arr = te_data['y_arr']
-			te_y_arr[:,0] = (te_y_arr[:,0] - tr_y1_mean)/tr_y1_std
-			te_y_arr[:,1] = (te_y_arr[:,1] - tr_y2_mean)/tr_y2_std
+			print("Normalizing y test data (y1)")
+			normalize(te_y_arr[:,0], dryrun=True)
+			te_y_arr[:,0] = (te_y_arr[:,0] - train_data.tr_y1_mean) / train_data.tr_y1_std
+			print("Normalizing y test data (y2)")
+			normalize(te_y_arr[:,1], dryrun=True)
+			te_y_arr[:,1] = (te_y_arr[:,1] - train_data.tr_y2_mean) / train_data.tr_y2_std
 			self.y_arr = te_y_arr
-			del te_data, te_y_arr
-		# convert to tensors
-		del tr_data, tr_y_arr
-		self.y_arr = torch.tensor(self.y_arr, device=device, dtype=torch.float32)
-
+			self.y_arr = torch.tensor(self.y_arr, device=device, dtype=torch.float32)
+   
 	def __len__(self):
 		return self.size
 
 	def __getitem__(self, idx):
 		return self.x_arr[idx], self.y_arr[idx]
-
-
 
 def gen_sample(distributions,ifo, ml_model, tosample=['f0', 'gbw', 'amp_r', 'amp_i', 'time']):
     res = {key:None for key in tosample}
--- notebooks/cvnn_models.py
+++ notebooks/cvnn_models.py
@@ -6,85 +6,60 @@ class ComplexValuedNN(nn.Module):
     """ This class defines a complex-valued neural network model. 
     It has two complex-valued convolutional layers, two complex-valued fully connected layers,
     with fully complex activations throughout the network. The final output layer is real-valued."""
-    def __init__(self):
+    def __init__(self, n_conv_layers=2, conv_filters=[4,8], conv_kernel_size=[1,3], n_fc_layers=2, n_fc_units=128):
         super(ComplexValuedNN, self).__init__()
+        self.conv_layers = nn.ModuleList()
+        self.fc_layers = nn.ModuleList()
+        for i in range(n_conv_layers):
+            if i == 0:
+                self.conv_layers = nn.ModuleList([cvtorch.nn.CVConv1d(1, conv_filters[i], conv_kernel_size[i], padding="same")])
+            else:
+                self.conv_layers.append(cvtorch.nn.CVConv1d(conv_filters[i-1], conv_filters[i], conv_kernel_size[i], padding="same"))
+            self.conv_layers.append(cvtorch.nn.CVCardiod())
+            self.conv_layers.append(cvtorch.nn.CVBatchNorm1d(conv_filters[i]))
+            # self.conv_layers.append(cvtorch.nn.CVDropout(0.25))
 
-        # Complex Conv Layer 1
-        self.conv1 = cvtorch.nn.CVConv1d(1, 4, 1, padding="same")
-        self.activation1 = cvtorch.nn.CVSigLog()
-        self.bn1 = cvtorch.nn.CVBatchNorm1d(4)
-        # self.dropout1 = cvtorch.nn.CVDropout(0.25)
-
-        # Complex Conv Layer 2
-        self.conv2 = cvtorch.nn.CVConv1d(4, 8, 3, padding="same")
-        self.activation2 = cvtorch.nn.CVSigLog()
-        self.bn2 = cvtorch.nn.CVBatchNorm1d(8)
-        # self.dropout2 = cvtorch.nn.CVDropout(0.25)
-
-        # Fully connected Layer 1
-        self.fc1 = cvtorch.nn.CVLinear(8 * 513, 128)
-        self.activation3 = cvtorch.nn.CVSigLog()
-        # self.bn3 = cvtorch.nn.CVBatchNorm1d(1024)
-        # self.dropout3 = cvtorch.nn.CVDropout(0.25)
-
-        # Fully connected Layer 2
-        # self.fc2 = cvtorch.nn.CVLinear(1024, 1024)
-        # self.activation4 = cvtorch.nn.zReLU()
-        # self.bn4 = cvtorch.nn.CVBatchNorm1d(1024)
-        # self.dropout4 = cvtorch.nn.CVDropout(0.25)
-        # Fully connected Layer 2
-        self.fc3 = cvtorch.nn.CVLinear(128, 32)
-        self.activation5 = cvtorch.nn.CVSigLog()
-        
-        # self.bn5 = cvtorch.nn.CVBatchNorm1d(32)
-        # self.dropout5 = cvtorch.nn.CVDropout(0.25)
-        
+        if n_conv_layers > 0:
+            self.flatten_size = conv_filters[-1] * 513  # Adjusted size to reflect conv2 output * sequence length
+        else:
+            self.flatten_size = 513
+            
+        for i in range(n_fc_layers):
+            if i == 0:
+                self.fc_layers = nn.ModuleList([cvtorch.nn.CVLinear(self.flatten_size, n_fc_units)])
+            else:
+                self.fc_layers.append(cvtorch.nn.CVLinear(n_fc_units, n_fc_units))
+            self.fc_layers.append(cvtorch.nn.CVCardiod())
+            # self.fc_layers.append(cvtorch.nn.CVBatchNorm1d(n_fc_units))
+            # self.fc_layers.append(cvtorch.nn.CVDropout(0.25))
+            
+            
         # Output Layer (real-valued)
-        self.output_layer = nn.Linear(32 * 2, 2)
 
+        self.output_layer = nn.Linear(n_fc_units * 2, 2)
+        
     def forward(self, x):
         # Add channel dimension for convolution
-        x = x.unsqueeze(1)
-        # x = x.reshape(x.shape[1], 1, -1)
-
-        # Conv Layer 1
-        x = self.conv1(x)
-        x = self.activation1(x)
-        x = self.bn1(x)
-        # x = self.dropout1(x)
-
-        # # # Conv Layer 2
-        x = self.conv2(x)
-        x = self.activation2(x)
-        x = self.bn2(x)
-        # x = self.dropout2(x)
-
+        if len(self.conv_layers) > 0:
+            x = x.unsqueeze(1)
+        
+        for layer in self.conv_layers:
+            x = layer(x)
+            
         # Flatten the tensor
-        x = x.view(x.size(0), -1)
-
-        # Fully Connected Layer 1
-        x = self.fc1(x)
-        x = self.activation3(x)
-        # x = self.bn3(x)
-        # x = self.dropout3(x)
-
-        # Fully Connected Layer 2
-        # x = self.fc2(x)
-        # x = self.activation4(x)
-        # x = self.bn4(x)
-        # x = self.dropout4(x)
-        x = self.fc3(x)
-        x = self.activation5(x)
-        # x = self.bn5(x)
-        # x = self.dropout5(x)
+        if len(self.conv_layers) > 0:
+            x = x.view(x.size(0), -1)
+        
+        for layer in self.fc_layers:
+            x = layer(x)
+        
         # Transform complex valued output for the final real-valued layer
         real_x = torch.cat((x.real, x.imag), dim=1)  # Merge real and imag parts
-
+        
         # Output Layer
         out = self.output_layer(real_x)
-
+        
         return out
-
 
 class RealValuedNN(nn.Module):
     """ This class defines a real-valued neural network model.
@@ -92,79 +67,58 @@ class RealValuedNN(nn.Module):
     with real-valued activations throughout the network. The final output layer is real-valued.
     
     The real and imaginary parts of the data are combined as separate channels."""
-    def __init__(self):
+    def __init__(self, n_conv_layers=2, conv_filters=[8,16], conv_kernel_size=[1,3], n_fc_layers=2, n_fc_units=128):
         super(RealValuedNN, self).__init__()
+        self.conv_layers = nn.ModuleList()
+        self.fc_layers = nn.ModuleList()
+        for i in range(n_conv_layers):
+            if i == 0:
+                self.conv_layers = nn.ModuleList([nn.Conv1d(2, conv_filters[i], conv_kernel_size[i], padding="same")])
+            else:
+                self.conv_layers.append(nn.Conv1d(conv_filters[i-1], conv_filters[i], conv_kernel_size[i], padding="same"))
+            self.conv_layers.append(nn.ReLU())
+            self.conv_layers.append(nn.BatchNorm1d(conv_filters[i]))
+            # self.conv_layers.append(nn.Dropout(0.25))
 
-        # Conv Layer 1
-        self.conv1 = nn.Conv1d(2, 8, kernel_size=1, padding='same')
+        if n_conv_layers > 0:
+            self.flatten_size = conv_filters[-1] * 513
+        else:
+            self.flatten_size = 513 *2
 
-        # Activation 1
-        self.activation1 = nn.ReLU()
-        
-        # BatchNorm 1
-        self.bn1 = nn.BatchNorm1d(8)
-
-        # Conv Layer 2
-        self.conv2 = nn.Conv1d(8, 16, kernel_size=3, padding='same')
-
-        # Activation 2
-        self.activation2 = nn.ReLU()
-        
-        # BatchNorm 2
-        self.bn2 = nn.BatchNorm1d(16)
-
-        # Given input size is (513), Convolution does not alter the dimension with padding='same'
-        # Therefore, the size is the same after convolutions (513).
-        # Flatten the tensor
-        self.flatten_size = 16 * 513  # Adjusted size to reflect conv2 output * sequence length
-
-        # Fully connected Layer 1
-        self.fc1 = nn.Linear(self.flatten_size, 128)
-
-        # Activation 3
-        self.activation3 = nn.ReLU()
-
-        # Fully connected Layer 2
-        self.fc2 = nn.Linear(128, 32)
-
-        # Activation 4
-        self.activation4 = nn.ReLU()
+        for i in range(n_fc_layers):
+            if i == 0:
+                self.fc_layers = nn.ModuleList([nn.Linear(self.flatten_size, n_fc_units)])
+            else:
+                self.fc_layers.append(nn.Linear(n_fc_units, n_fc_units))
+            self.fc_layers.append(nn.ReLU())
+            # self.fc_layers.append(nn.BatchNorm1d(n_fc_units))
+            # self.fc_layers.append(nn.Dropout(0.25))
         
         # Output Layer (real-valued)
-        self.output_layer = nn.Linear(32, 2)
-
+        self.output_layer = nn.Linear(n_fc_units, 2)
+        
     def forward(self, x):
         # Combine real and imaginary parts as separate channels
         real = x.real
         imag = x.imag
         x = torch.stack((real, imag), dim=1)
-
-        # Conv Layer 1
-        x = self.conv1(x)
-        x = self.activation1(x)
-        x = self.bn1(x)
-
-        # Conv Layer 2
-        x = self.conv2(x)
-        x = self.activation2(x)
-        x = self.bn2(x)
-
-        # Flatten the tensor
-        x = x.view(x.size(0), -1)
         
-        # Fully Connected Layer 1
-        x = self.fc1(x)
-        x = self.activation3(x)
-
-        # Fully Connected Layer 2
-        x = self.fc2(x)
-        x = self.activation4(x)
-
+        for layer in self.conv_layers:
+            x = layer(x)
+            
+        # Flatten the tensor
+        if len(self.conv_layers) > 0:
+            x = x.view(x.size(0), -1)
+        
+        for layer in self.fc_layers:
+            x = layer(x)
+        
         # Output Layer
         out = self.output_layer(x)
-
+        
         return out
-    
+        
+
 class RealValuedNNtd(nn.Module):
     """ This class defines a real-valued neural network model.
     It has two real-valued convolutional layers, two real-valued fully connected layers,
